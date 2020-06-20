f39efd65c3df070dba3215dca6491850b79b040f
tuzixiangs <1633875789@qq.com>
2020-06-20 21:22:49 +0800

    添加进度条功能

--- src/main.js
+++ src/main.js
@@ -6,12 +6,25 @@ import "./plugins/element.js";
 import "assets/css/global.css";
 import ZkTable from "vue-table-with-tree-grid";
 
+// 导入进度条控制器
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
 // 设置默认的请求根路径
 import axios from "axios";
 axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/";
 // 添加请求拦截器，为每次请求添加Authorization字段
+// 在request拦截器中，展示进度条
 axios.interceptors.request.use((config) => {
+	// 展示进度条
+	NProgress.start();
 	config.headers.Authorization = window.sessionStorage.getItem("token");
+	return config;
+});
+// 在response拦截器中，隐藏进度条
+axios.interceptors.response.use((config) => {
+	// 隐藏进度条
+	NProgress.done();
 	return config;
 });
 Vue.prototype.$http = axios;
@@ -22,7 +35,7 @@ import VueQuillEditor from "vue-quill-editor";
 import "quill/dist/quill.core.css"; // import styles
 import "quill/dist/quill.snow.css"; // for snow theme
 import "quill/dist/quill.bubble.css"; // for bubble theme
-// 将富文本编辑器注册未全局可用的组件
+// 将富文本编辑器注册为全局可用的组件
 Vue.use(VueQuillEditor);
 
 Vue.config.productionTip = false;
